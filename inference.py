66fd2704e84d51fe36ab537907777f1edc4c92b4
VladaTrus <47668051+VladaTrus@users.noreply.github.com>
2020-12-13 22:54:29 +0300

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,28 @@
+import torch
+import torchaudio
+import numpy as np
+from model import WaveNet, MuLaw
+from featurizer_spec import MelSpectrogram, MelSpectrogramConfig
+
+
+mulaw = MuLaw()
+featurizer = MelSpectrogram(MelSpectrogramConfig()).to(device)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = WaveNet()
+model.load_state_dict(torch.load('wavenet_15.pth'))
+model.to(device)
+model.eval()
+
+wav, _ = torchaudio.load("LJ033-0047.wav")
+wav = wav.to(device)
+
+model.eval()
+with torch.no_grad():
+    mels = featurizer(wav)
+    prediction = model.inference(mels).squeeze()
+
+result = mulaw.decode(prezz).cpu()
+torchaudio.save('result.wav', result, 22050)
+ 
